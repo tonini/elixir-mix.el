7ec459b3fab2bb90acdebbf44f9da9b2765a5b99
Samuel Tonini <tonini.samuel@gmail.com>
2013-07-15 20:44:53 +0200

    pump v0.0.3

--- elixir-mix.el
+++ elixir-mix.el
@@ -5,7 +5,7 @@
 ;; Author: Samuel Tonini
 ;; Maintainer: Samuel Tonini
 ;; Created: So Jun  9 10:01:02 2013 (+0200)
-;; Version: 0.0.2
+;; Version: 0.0.3
 ;; URL: http://github.com/tonini/elixir-mix.el
 ;; Keywords: elixir, mix, elixir-mix
 
